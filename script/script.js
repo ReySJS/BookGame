919d31f85073ea6694896c6741da899cc98d4aff
Rey <rey.filareto@outlook.com>
2021-06-10 12:41:29 -0300

    Submit Application

---
+++ script/script.js
@@ -0,0 +1,149 @@
+const livros = [{
+  nome: "O Alquimista",
+  paginas:
+  {
+    1: "Não existe nada de completamente errado no mundo, mesmo um relógio parado consegue estar certo duas vezes por dia.",
+    2: "Tudo que acontece uma vez poderá nunca mais acontecer, mas tudo o que acontece duas vezes, certamente acontecerá uma terceira.",
+    3: "Muito cuidado com os elogios rápidos. Aqueles que, na primeira tarde, são capazes de ver qualidades que não tens, também rapidamente descobrem defeitos que nunca possuístes"
+  }, capa: "images/o_alquimista.jpg"
+},
+{
+  nome: "O Silmarillion",
+  paginas:
+  {
+    1: "É que da bem-aventurança e da alegria na vida há pouco a ser dito enquanto duram; assim como as obras belas e maravilhosas, enquanto perduram para que os olhos as contemplem, são registros de si mesmas; e somente quando correm perigo ou são destruídas é que se transformam em poesia.",
+    2: "Amigo ou imigo, com mácula ou limpo, cria de Morgoth ou claro Vala, Elda ou Maia ou a Outra Raça, Mortal que há de vir na Terra-média, nem lei, nem amor, nem liga de espadas, medo ou muro, nem mesmo o Destino será defesa contra Fëanor, e os filhos de Fëanor, para o que nas mãos tomar, ou em seu meio ocultar, consigo guardar ou lançar longe uma Silmaril. Assim dizemos nós: Morte lhe traremos quando termine o Dia, mal até o fim do mundo! O juramento ouvi, Eru Pai-de-Todos! À treva eternal condenai a nós se não o cumprirmos. No sacro monte sede testemunhas e a promessa lembrai, Manwë e Varda!",
+    3: "Mas quando o exército de Fingolfin entrou em Mithrim, o Sol ergueu-se flamejante no Ocidente; e Fingolfin desfraldou as suas bandeiras, azuis e prateadas, e tocou as suas trompas, e irromperam flores sob os seus pés em marcha e as eras das estrelas terminaram."
+  }, capa: "images/o_silmarillion.jpg"
+}, {
+  nome: "A Arte da Guerra",
+  paginas:
+  {
+    1: "Se você conhece o inimigo e conhece a si mesmo, não precisa temer o resultado de cem batalhas. Se você se conhece mas não conhece o inimigo, para cada vitória ganha sofrerá também uma derrota. Se você não conhece nem o inimigo nem a si mesmo, perderá todas as batalhas.",
+    2: "Aquele que se empenha a resolver as dificuldades resolve-as antes que elas surjam. Aquele que se ultrapassa a vencer os inimigos triunfa antes que as suas ameaças se concretizem.",
+    3: "Seja extremamente sutil, tão sutil que ninguém possa achar qualquer rastro. Seja extremamente misterioso, tão misterioso que ninguém possa ouvir qualquer informação. Se um general puder agir assim, então, poderá celebrar o destino do inimigo em suas próprias mãos."
+  }, capa: "images/a_arte_da_guerra.jpg"
+}, {
+  nome: "Uma Abreviada",
+  paginas:
+  {
+    1: "Um universo em expansão não impede que haja um criador, mas impõe limites sobre quando esse trabalho pode ter sido executado.",
+    2: "Mesmo as pessoas que dizem que tudo está predeterminado e que não podemos fazer nada para mudá-lo, olham para os dois lados antes de atravessar a rua.",
+    3: "Se houve eventos anteriores a esse momento, eles não puderam afetar o que acontece no presente. Sua existência pode ser ignorada porque não teria consequências observacionais. Pode-se dizer que o tempo teve início no Big Bang, no sentido de que tempos anteriores simplesmente não teriam defi nição."
+  }, capa: "images/uma_breve_historia_do_tempo.jpg"
+}, {
+  nome: "Ponto de Impacto",
+  paginas:
+  {
+    1: "Se esta descoberta for confirmada, com certeza será uma das mais incríveis revelações sobre nosso universo já feitas pela ciência. Suas implicações são tão vastas e impressionantes que ultrapassam nossa imaginação. Ao mesmo tempo que promete responder a algumas de nossas mais antigas perguntas, ela nos coloca diante de outras ainda mais fundamentais.",
+    2: "Se esta descoberta for confirmada, com certeza será uma das mais incríveis revelações sobre nosso universo já feitas pela ciência. Suas implicações são tão vastas e impressionantes que ultrapassam nossa imaginação. Ao mesmo tempo que promete responder a algumas de nossas mais antigas perguntas, ela noscoloca diante de outras ainda mais fundamentais.",
+    3: "Se esta descoberta for confirmada, com certeza será uma das mais incríveis revelações sobre nosso universo já feitas pela ciência. Suas implicações são tão vastas e impressionantes que ultrapassam nossa imaginação. Ao mesmo tempo que promete responder a algumas de nossas mais antigas perguntas, ela nos coloca diante de outras ainda mais fundamentais."
+  }, capa: "images/ponto_de_impacto.jpg"
+},
+{
+  nome: "O Retorno do Rei",
+  paginas:
+  {
+    1: "A velha sabedoria do Ocidente foi esquecida. Os reis fizeram tumbas mais belas que as casas dos vivos, dando mais valor ao nome de seus ancestrais do que aos dos seus filhos.",
+    2: "Poderá haver um dia em que a coragem dos homens termine quando desertamos nossos amigos e trairmos os laços de amizade, mas esse dia não é hoje, uma hora de lobo e escudos despedaçados quando a era dos homens for destruída, mas esse dia não é hoje... Hoje nós lutamos... por tudo que amam nessa bela terra eu pesso que resistam!!",
+    3: "Das cinzas uma fogueira vai despertar. Uma luz das sombras vai aparecer. A espada que foi quebrada vai se restaurar. Quem não tem coroa, Rei voltará a ser."
+  }, capa: "images/o_retorno_do_rei.jpg"
+}, {
+  nome: "Norte e Sul",
+  paginas:
+  {
+    1: "Está bem. Mas fabricar carruagens é comércio do mesmo jeito, e acho até que é muito mais inútil que o dos açougueiros ou padeiros. Oh! Como costumava me cansar dos passeios diários de carruagem com a Tia Shaw, e como desejava caminhar!",
+    2: "Olhou pela janela, na direção da silhueta cinza escura da torre da igreja, que se elevava quadrada e ereta no seu campo de visão, recortada contra as sombras de um azul transparente mais ao longe. Seu olhar foi atraído para lá. Sentiu que podia olhar para sempre, cada vez mais longe",
+    3: "– É verdade. Passei os olhos por esse livro quando estava na escola. Confesso que até o considero um clássico muito bonito hoje em dia, embora o latim e o grego já tenham saído da minha memória desde então. Mas lhe pergunto: que preparação ele me deu para a vida que levo hoje? Nenhuma. Exatamente nenhuma. No que refere à educação, qualquer homem que saiba ler e escrever está no mesmo nível que eu,"
+  }, capa: "images/norte_e_sul.jpg"
+}, {
+  nome: "Eu, Robô",
+  paginas:
+  {
+    1: "Eu não aceito afirmações apoiadas em autoridade. Uma hipótese deve ser sustentada pela razão, de outro modo é inútil... e contraria todos os preceitos da lógica supor que vocês me criaram.",
+    2: "Se o conhecimento pode criar problemas, não é através da ignorância que podemos solucioná-los.",
+    3: "O aspecto mais triste da vida de hoje é que a ciência ganha em conhecimento mais rapidamente que a sociedade em sabedoria."
+  }, capa: "images/eu_robo.jpg"
+}];
+
+const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'];
+
+function mostrarOpcoesDeLivros() {
+  for (let i = 0; i < livros.length; i++) {
+    document.getElementById('livros').innerHTML += `
+  <p class="livros_disponiveis"> 
+  <img onclick="mostrarLivroSelecionado(${i})" src="${livros[i].capa}" />
+  <span>${livros[i].nome}</span>
+  </p>`
+  }
+}
+
+mostrarOpcoesDeLivros();
+
+let paginaSorteada;
+let letraSorteada;
+
+function mostrarLivroSelecionado(indexDoLivro) {
+  let indexDaPagina = sortearPagina(3)
+  let letra = sortearLetra(26);
+  document.getElementById('pagina_sorteada').textContent = indexDaPagina
+  document.getElementById('livro_selecionado_img').src = `${livros[indexDoLivro].capa}`;
+  document.getElementById('livro_selecionado_span').style.display = 'none';
+  document.getElementById('letra_sorteada').textContent = alfabeto[letra].toUpperCase();
+  document.getElementById('entrada_de_texto').innerHTML = livros[indexDoLivro].paginas[indexDaPagina];
+  contarLetraNaPagina(livros[indexDoLivro].paginas[indexDaPagina], alfabeto[letra]);
+  paginaSorteada = indexDaPagina;
+  letraSorteada = letra;
+}
+
+function sortearPagina(max) {
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max)) + 1;
+}
+
+function sortearLetra(max) {
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max)) + 0;
+}
+
+var cont = 0;
+
+function contarLetraNaPagina(paginaSorteada, letraSorteada) {
+  cont = 0;
+  console.log(paginaSorteada)
+  for (let i = 0; i < paginaSorteada.length; i++) {
+    if (paginaSorteada[i] == letraSorteada) {
+      cont++
+    }
+  }
+  return cont;
+}
+
+function jogar(palpite_do_jose, palpite_do_caio) {
+  let campoDeResultado = document.getElementById('resultado_do_jogo');
+  if (paginaSorteada == undefined) {
+    campoDeResultado.innerHTML = "Escolha um Livro"
+  } else if (palpite_do_jose == "" || palpite_do_caio == "") {
+    campoDeResultado.innerHTML = "Palpites não Inseridos"
+  } else if (palpite_do_jose < 0 || palpite_do_caio < 0) {
+    campoDeResultado.innerHTML = "Palpite Inválido"
+  } else if (palpite_do_jose == palpite_do_caio) {
+    campoDeResultado.innerHTML = "Insira Palpites Diferentes"
+  } else {
+
+    document.getElementById('quantidade_letra_sorteada').innerHTML = cont;
+
+    let resultadoDoJose = Math.abs(cont - palpite_do_jose);
+    let resultadoDoCaio = Math.abs(cont - palpite_do_caio);
+
+    if (resultadoDoJose == resultadoDoCaio) {
+      document.getElementById('resultado_do_jogo').innerHTML = `Empate!<br />A letra "${alfabeto[letraSorteada].toUpperCase()}" teve "${cont}" ocorrências no texto.`
+    } else
+      if (resultadoDoJose > resultadoDoCaio) {
+        document.getElementById('resultado_do_jogo').innerHTML = `Caio Venceu!<br />A letra "${alfabeto[letraSorteada].toUpperCase()}" teve "${cont}" ocorrências no texto.`
+      }
+      else {
+        document.getElementById('resultado_do_jogo').innerHTML = `José Venceu!<br />A letra "${alfabeto[letraSorteada].toUpperCase()}" teve "${cont}" ocorrências no texto.`
+      }
+  }
+}
